276a35484eb0ad8cad5f2919b0af7543ecb5d8d8
uttejkalluri2005 <137720609+uttejkalluri2005@users.noreply.github.com>
2025-03-11 21:52:14 +0530

    Update recomender.py

--- recomender.py
+++ recomender.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 app = Flask(__name__)
 fin_pt = pickle.load(open("fin_pt.pkl","rb"))
-books = pickle.load(open("books (1).pkl","rb"))
+books = pickle.load(open("books.pkl","rb"))
 @app.route("/",methods=["POST","GET"])
 def recommend():
     book_name = request.form.get("book_input")
